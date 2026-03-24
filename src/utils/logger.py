1641713bdeda93176a144e9156e3b4bdc9b77b07
joaoaugustolaner <joaoaugustolaner@proton.me>
2026-03-24 17:36:47 -0300

    Addopt pdm for package manager

---
+++ src/utils/logger.py
@@ -0,0 +1,14 @@
+import logging
+import sys
+
+LOGGING_FORMAT = '%(asctime)s - [%(levelname)s] - %(message)s'
+DATE_FORMAT = '%Y - %m - %d'
+
+logging.basicConfig(
+    level=logging.DEBUG,
+    format=LOGGING_FORMAT,
+    datefmt=DATE_FORMAT,
+    handlers=[logging.StreamHandler(sys.stderr)],
+)
+
+logger = logging.getLogger('logger')
